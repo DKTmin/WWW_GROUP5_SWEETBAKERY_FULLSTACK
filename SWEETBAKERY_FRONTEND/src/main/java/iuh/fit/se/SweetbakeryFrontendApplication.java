f05cd5100b6f0ae0fd3baa9a9427dfa1ae9593d0
Dark Thank <dktran004@gmail.com>
2025-11-08 16:51:26 +0700

    KHOI TAO DU AN

---
+++ SWEETBAKERY_FRONTEND/src/main/java/iuh/fit/se/SweetbakeryFrontendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetbakeryFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetbakeryFrontendApplication.class, args);
+    }
+
+}
