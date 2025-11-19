f893b2e9f8aee9991d62ee213cdf025e1e8cde4f
phatdang <phatdang19052004@gmail.com>
2025-11-19 10:53:45 +0700

    refactor(dto.response.apiresponse): cấu hình exclude non-null field

--- SWEETBAKERY_BACKEND/src/main/java/iuh/fit/se/dtos/response/ApiResponse.java
+++ SWEETBAKERY_BACKEND/src/main/java/iuh/fit/se/dtos/response/ApiResponse.java
@@ -1,5 +1,7 @@
 package iuh.fit.se.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,6 +17,7 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse <T>{
     String message;
     Integer code;
