3948344d84ba6505f015fbe356bfdd89d2de1b9f
phatdang <phatdang19052004@gmail.com>
2025-11-19 10:54:37 +0700

    refactor(enums.HttpCode): thêm enum class HttpCode
    Cấu hình lỗi trả về client

---
+++ SWEETBAKERY_BACKEND/src/main/java/iuh/fit/se/entities/enums/HttpCode.java
@@ -0,0 +1,26 @@
+package iuh.fit.se.entities.enums;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 19/11/2025, Wednesday
+ **/
+
+@Getter
+public enum HttpCode {
+    OK(200, "Successfully!", HttpStatus.OK),
+    NOT_FOUND(404, "Not found!", HttpStatus.NOT_FOUND);
+    final int CODE;
+    final String MESSAGE;
+    final HttpStatusCode HTTP_CODE;
+
+    private HttpCode(int code, String message, HttpStatusCode httpStatusCode){
+        this.CODE = code;
+        this.MESSAGE = message;
+        this.HTTP_CODE = httpStatusCode;
+    }
+}
