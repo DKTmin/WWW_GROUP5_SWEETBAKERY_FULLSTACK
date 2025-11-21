0d5adba460dd08ba9dd7ca464057812fb904c85c
phatdang <phatdang19052004@gmail.com>
2025-11-21 21:40:18 +0700

    feat(enums): thêm enum class ValidationMessage để quản lý thông báo lỗi validate

---
+++ SWEETBAKERY_BACKEND/src/main/java/iuh/fit/se/entities/enums/ValidationMessage.java
@@ -0,0 +1,20 @@
+package iuh.fit.se.entities.enums;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 21/11/2025, Friday
+ **/
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum ValidationMessage {
+    PASSWORD_INVALID("Password must be at least {min} chars!");
+    String MESSAGE;
+    ValidationMessage(String message){
+        this.MESSAGE = message;
+    }
+}
