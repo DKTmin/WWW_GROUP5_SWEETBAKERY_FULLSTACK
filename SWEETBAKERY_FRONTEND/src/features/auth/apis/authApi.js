ec46ea926d265a7dec4da2207be2e5f9871a7b5a
phatdang <phatdang19052004@gmail.com>
2025-12-03 18:23:25 +0700

    feat: thêm chức năng cho phép khách hàng chỉnh sửa thông tin cá nhân

--- SWEETBAKERY_FRONTEND/src/features/auth/apis/authApi.js
+++ SWEETBAKERY_FRONTEND/src/features/auth/apis/authApi.js
@@ -4,11 +4,13 @@ const authApi = {
   login: (data) => axiosClient.post("/auth-management/api/v1/auth/log-in", data),
   register: (data) => axiosClient.post("/customer-management/api/v1/customers/register", data),
   refresh: (token) =>
-    axiosClient.post("/authentication-management/api/v2/auth/refresh", {
+    axiosClient.post("/auth-management/api/v1/auth/refresh", {
       token,
     }),
   updateInformation: (data) => axiosClient.put("/user-management/api/v1/users/infor", data),
   getInformation: () => axiosClient.get("/user-management/api/v1/users/information"),
+  updateCustomerInformation: (data, userId) =>
+    axiosClient.post(`/customer-management/api/v1/customers/update/${userId}`, data),
 };
 
 export default authApi;
